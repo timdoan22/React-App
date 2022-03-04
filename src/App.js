3d86bd79085ad2b7b7f0c4a10d7b53243e8410d6
timdoan22 <timdoan01@gmail.com>
2022-03-05 00:00:00 +0900

    cleaned up header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from './components/Header'
 
-function App() {
+const App = () => {
   return (
     <div className='container'>
       <Header />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+
+const Header = ({ title }) => {
   return (
-    <h1>Task Tracker</h1>
+    <header>
+        <h1>{title}</h1>
+    </header>
   )
 }
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// You can style things inline with variables
+// <h1 style={headingStyle}>title}</h1>
+// const headingStyle = {
+//     color: 'red', 
+//     backgroundColor: 'teal'
+// }
 
 export default Header
