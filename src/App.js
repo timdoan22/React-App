35b73b672fb2a38742252959adcdd30de44b33cc
timdoan22 <timdoan01@gmail.com>
2022-05-13 00:00:00 +0900

    toggle reminder complete

--- src/App.js
+++ src/App.js
@@ -24,16 +24,27 @@ const App = () => {
           "id": 4
         }
     ])
-  
+    
+    // Delete Task
     const deleteTask = (id) => {
       setTasks(tasks.filter((task) => task.id !== id))
+    }
+
+    // Toggle Reminder
+    const toggleReminder = (id) => {
+      setTasks(
+        tasks.map((task) => 
+          task.id === id ? {...task, reminder: !task.reminder} : task
+        )
+      )
     }
 
   return (
     <div className='container'>
       <Header />
       {tasks.length > 0 ? <Tasks tasks={tasks}  
-      onDelete={deleteTask}/> : 'No tasks to display'}
+      onDelete={deleteTask}
+      onToggle={toggleReminder}/> : 'No tasks to display'}
     </div>
   );
   }
