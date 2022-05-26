bf8030ce0fb306d8f8bcbf6bca73bbc20a181c85
timdoan22 <timdoan01@gmail.com>
2022-05-27 00:00:00 +0900

    import useState and set tasks as a state in App.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,33 +1,11 @@
-const tasks = [
-    {
-      "id": 1,
-      "text": "test",
-      "other": "no-show",
-      "day": "monday",
-      "reminder": true
-    },
-    {
-      "text": "Test4",
-      "day": "Wednesday",
-      "reminder": false,
-      "id": 3
-    },
-    {
-      "text": "Task6",
-      "day": "Thursday",
-      "reminder": true,
-      "id": 4
+const Tasks = ({ tasks }) => {
+    return (
+        <>
+            {tasks.map((task) => (
+                <h3 key={task.id}>{task.text}</h3>
+            ))}
+        </>
+        )
     }
-]
-
-const Tasks = () => {
-  return (
-    <>
-        {tasks.map((task) => (
-            <h3>{task.text}</h3>
-        ))}
-    </>
-  )
-}
 
 export default Tasks
