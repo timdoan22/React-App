40bb367072cbffbedb0df79b7e60ec663e43f017
timdoan22 <timdoan01@gmail.com>
2022-08-15 00:00:00 +0900

    delete button complete

--- src/App.js
+++ src/App.js
@@ -24,12 +24,18 @@ const App = () => {
           "id": 4
         }
     ])
+  
+    const deleteTask = (id) => {
+      setTasks(tasks.filter((task) => task.id !== id))
+    }
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? <Tasks tasks={tasks}  
+      onDelete={deleteTask}/> : 'No tasks to display'}
     </div>
   );
-}
+  }
 
 export default App
